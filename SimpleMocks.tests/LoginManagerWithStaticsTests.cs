777d08c3171e01e27f8d4a21a64ea85e0972072a
Goedebuur <Arjen.Goedegebure@gmail.com>
2014-09-09 14:09:14 +0200

    Assignment 9
    Test that IsLoginOK calls the logger's static write method

---
+++ SimpleMocks.tests/LoginManagerWithStaticsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MyBillingProduct;
+
+using NUnit.Framework;
+
+namespace SimpleMocks.tests
+{
+    [TestFixture]
+    public class LoginManagerWithStaticsTests
+    {
+        private static string SomePassword
+        {
+            get { return "testPassword"; }
+        }
+
+        private static string SomeUser
+        {
+            get { return "testUser"; }
+        }
+
+        [Test]
+        public void IsLoginOK_KnownUserKnownPassword_LogsOK()
+        {
+            //Arrange
+            TestableLoginManagerWithStatics testableLoginManager = new TestableLoginManagerWithStatics();
+            testableLoginManager.AddUser(SomeUser, SomePassword);
+
+            //Act
+            testableLoginManager.IsLoginOK(SomeUser, SomePassword);
+
+            //Assert
+            StringAssert.Contains(string.Format("login ok: user: {0}", SomeUser), testableLoginManager.CallLogText);
+
+        }
+    }
+
+    class TestableLoginManagerWithStatics : LoginManagerWithStatics
+    {
+        public string CallLogText = string.Empty;
+
+        protected override void CallLog(string text)
+        {
+            CallLogText = text;
+        }
+    }
+}
--- SimpleMocks/4_LoginManagerWithStatics.cs
+++ SimpleMocks/4_LoginManagerWithStatics.cs
@@ -6,33 +6,40 @@ namespace MyBillingProduct
 {
 	public class LoginManagerWithStatics
 	{
-	    private Hashtable m_users = new Hashtable();
+		private Hashtable m_users = new Hashtable();
 
-	    public bool IsLoginOK(string user, string password)
-	    {
-	        try
-	        {
-	           StaticLogger.Write("blah");
-	        }
-	        catch (LoggerException e)
-	        {
-	            StaticWebService.Write(e.Message + Environment.MachineName);
-	        }
-	        if (m_users[user] != null &&
-	            (string) m_users[user] == password)
-	        {
-	            return true;
-	        }
-	        return false;
-	    }
+		public bool IsLoginOK(string user, string password)
+		{
+			try
+			{
+			   CallLog("blah");
+			}
+			catch (LoggerException e)
+			{
+				StaticWebService.Write(e.Message + Environment.MachineName);
+			}
+			if (m_users[user] != null &&
+				(string) m_users[user] == password)
+			{
+			    CallLog(
+			        string.Format("login ok: user: {0}", user));
+				return true;
+			}
+			return false;
+		}
+
+		protected virtual void CallLog(string text)
+		{
+			StaticLogger.Write(text);
+		}
 
 
-	    public void AddUser(string user, string password)
-	    {
-	        m_users[user] = password;
-	    }
+		public void AddUser(string user, string password)
+		{
+			m_users[user] = password;
+		}
 
-	    public void ChangePass(string user, string oldPass, string newPassword)
+		public void ChangePass(string user, string oldPass, string newPassword)
 		{
 			m_users[user]= newPassword;
 		}
